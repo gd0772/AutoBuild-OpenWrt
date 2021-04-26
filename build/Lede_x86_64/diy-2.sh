01893d9d4a9b6d9fb0590a1c1c3691f280f688fb
gd0772 <61563545+gd0772@users.noreply.github.com>
2021-04-26 09:37:01 +0800

    AutoBuild-OpenWrt

--- build/Lede_x86_64/diy-2.sh
+++ build/Lede_x86_64/diy-2.sh
@@ -12,7 +12,7 @@ sed -i "/uci commit network/i\uci set network.lan.broadcast='192.168.123.255'" $
 sed -i "/uci commit network/i\uci set network.lan.dns='192.168.123.1'" $ZZZ                       # DNS(多个DNS要用空格分开)
 #sed -i "/uci commit network/i\uci set network.lan.delegate='0'" $ZZZ                             # 去掉LAN口使用内置的 IPv6 管理
 #echo "close_dhcp" > package/base-files/files/etc/closedhcp                                       # 关闭DHCP服务
-sed -i "s/R21.4.18/R21.4.18 $(TZ=UTC-8 date "+%Y.%m.%d") Build by gd772/g" $ZZZ                   #
+sed -i "s/R21.4.18/R21.4.18 $(TZ=UTC-8 date "+%Y.%m.%d") Build by gd772/g" $ZZZ                   # 添加固件编译日期及固件编译者
 #sed -i "/uci commit system/i\uci set system.@system[0].hostname='N1'" $ZZZ                       # 修改主机名称为N1
 
 # echo '替换系统文件'

07f01148ca4178563fd29f0579fb4f0e3a8f20db
gd0772 <61563545+gd0772@users.noreply.github.com>
2021-04-26 08:32:25 +0800

    AutoBuild-OpenWrt

--- build/common/LIENOL/files/etc/webweb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-sed -i 's/<%=pcdata(ver.distversion)%>/<%=pcdata(ver.distversion)%><!--/g' /usr/lib/lua/luci/view/admin_status/index.htm
-sed -i 's/(<%=pcdata(ver.luciversion)%>)/(<%=pcdata(ver.luciversion)%>)-->/g' /usr/lib/lua/luci/view/admin_status/index.htm
-sed -i '/webweb.sh/d' /etc/crontabs/root
-rm -rf /etc/webweb.sh

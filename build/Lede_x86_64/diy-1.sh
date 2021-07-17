4a101d06e7428dfda703b51e760e6add4d31cd60
gd0772 <admin@hysj.gd772.me>
2021-07-17 14:58:29 +0800

    AutoBuild-OpenWrt

--- build/Lede_x86_64/diy-1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# 空空如也 说啥也不好使了
--- build/Lede_x86_64/diy-2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# https://github.com/gd0772/AutoBuild-OpenWrt
-# common Module by gd0772
-
-# 空空如也 说啥也不好使了
-

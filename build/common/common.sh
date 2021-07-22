b10cc7106746de62ce03ff550463ea80b8bf839e
gd0772 <61563545+gd0772@users.noreply.github.com>
2021-07-23 03:00:14 +0800

    Update common.sh

--- build/common/common.sh
+++ build/common/common.sh
@@ -30,6 +30,7 @@ find . -name 'luci-app-netdata' -o -name 'netdata' -o -name 'luci-theme-argon' |
 
 git clone https://github.com/gd0772/package package/gd772
 rm -rf package/gd772/luci-app-pptp-server
+rm -rf package/gd772/luci-lib-docker
 curl -fsSL https://raw.githubusercontent.com/gd0772/patch/main/x86.sh | sh
 
 if [[ "${Modelfile}" == "Lede_source" ]]; then

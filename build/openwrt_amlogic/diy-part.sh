fe9040104b24910df18dc7b93ddf3281473a1de7
gd0772 <61563545+gd0772@users.noreply.github.com>
2021-10-21 05:42:11 +0800

    Update diy-part.sh

--- build/openwrt_amlogic/diy-part.sh
+++ build/openwrt_amlogic/diy-part.sh
@@ -5,6 +5,9 @@
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 # 修改IP项的EOF于EOF之间请不要插入其他扩展代码，可以删除或注释里面原本的代码
 
+# 修改 主机名为 N1
+sed -i "s/'OpenWrt'/'N1'/g" package/base-files/files/bin/config_generate
+
 # 设置打包固件的机型，内核组合（请看说明）
 cat >$GITHUB_WORKSPACE/amlogic_openwrt <<-EOF
 rootfs_size=640

887afe9f9c428ccd34f5ea3a8c05f0e5c9f2b0e3
gd0772 <61563545+gd0772@users.noreply.github.com>
2021-10-22 12:44:02 +0800

    Update diy-part.sh

--- build/openwrt_amlogic/diy-part.sh
+++ build/openwrt_amlogic/diy-part.sh
@@ -10,7 +10,7 @@ sed -i "s/'OpenWrt'/'N1'/g" package/base-files/files/bin/config_generate
 
 # 设置打包固件的机型，内核组合（请看说明）
 cat >$GITHUB_WORKSPACE/amlogic_openwrt <<-EOF
-rootfs_size=640
+rootfs_size=944
 amlogic_model=s905d
 amlogic_kernel=5.4.152
 EOF

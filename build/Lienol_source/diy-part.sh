ae9c7300f803010174cb530e1854418ea9b18673
gd0772 <61563545+gd0772@users.noreply.github.com>
2021-10-27 02:18:16 +0800

    AutoBuild-OpenWrt

--- build/Lienol_source/diy-part.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-# DIY扩展 在此脚本 增加插件
-
-# sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ                                                              # 设置密码为空

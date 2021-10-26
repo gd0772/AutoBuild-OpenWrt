63113797bdace75e48ebc02389eb60312bbaf33a
gd0772 <61563545+gd0772@users.noreply.github.com>
2021-10-26 15:40:26 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+# DIY扩展 在此脚本 增加插件
 
+# sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ                                                              # 设置密码为空

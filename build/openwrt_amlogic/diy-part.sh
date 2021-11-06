1644f82d9d3438acd26ad2704f1d909beeddcc2b
gd0772 <61563545+gd0772@users.noreply.github.com>
2021-11-07 05:02:25 +0800

    Update diy-part.sh

--- build/openwrt_amlogic/diy-part.sh
+++ build/openwrt_amlogic/diy-part.sh
@@ -4,11 +4,11 @@
 
 # sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ                                                            # 设置密码为空
 
-sed -i "s/'OpenWrt'/'N1'/g" package/base-files/files/bin/config_generate                            # 设置主机名
+  sed -i "s/'OpenWrt'/'R4S'/g" package/base-files/files/bin/config_generate                            # 设置主机名
 
 # 设置打包固件的机型，内核组合（可用内核是时时变化的,过老的内核就删除的，所以要选择什么内核请看说明)
-cat >$GITHUB_WORKSPACE/amlogic_openwrt <<-EOF
-amlogic_model=s905d
-amlogic_kernel=5.4.155_5.14.15
-rootfs_size=700
-EOF
+# cat >$GITHUB_WORKSPACE/amlogic_openwrt <<-EOF
+# amlogic_model=s905d
+# amlogic_kernel=5.4.155_5.14.15
+# rootfs_size=700
+# EOF

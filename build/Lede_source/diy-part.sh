aef2d8f68d4073204007134869e59bb630fdd83d
gd0772 <61563545+gd0772@users.noreply.github.com>
2021-12-17 22:29:22 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -2,4 +2,6 @@
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 # DIY扩展 在此脚本 增加插件
 
-# sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ                                                              # 设置密码为空
+git clone https://github.com/gd0772/package package/gd772
+wget https://raw.githubusercontent.com/gd0772/patch/main/gd772.sh
+bash gd772.sh

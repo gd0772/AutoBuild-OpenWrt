4779e8e81a072c52c66ec236fdaa9cf3e481a02c
gd0772 <61563545+gd0772@users.noreply.github.com>
2021-12-17 22:30:29 +0800

    Update diy-part.sh

--- build/openwrt_amlogic/diy-part.sh
+++ build/openwrt_amlogic/diy-part.sh
@@ -2,9 +2,9 @@
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 # DIY扩展二合一了，在此处可以增加插件
 
-# sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ                                                            # 设置密码为空
-
-sed -i "s/'OpenWrt'/'N1'/g" package/base-files/files/bin/config_generate                            # 设置主机名
+git clone https://github.com/gd0772/package package/gd772
+wget https://raw.githubusercontent.com/gd0772/patch/main/n1.sh
+bash n1.sh
 
 # 设置打包固件的机型，内核组合（可用内核是时时变化的,过老的内核就删除的，所以要选择什么内核请看说明)
 cat >$GITHUB_WORKSPACE/amlogic_openwrt <<-EOF

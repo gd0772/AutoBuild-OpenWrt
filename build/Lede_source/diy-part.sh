86d77cd23b600197705f2a4ea7ecdcfe87c0b1c1
gd0772 <61563545+gd0772@users.noreply.github.com>
2022-03-25 21:04:25 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -2,6 +2,5 @@
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 # DIY扩展 在此脚本 增加插件
 
-git clone https://github.com/gd0772/package package/gd772
 wget https://raw.githubusercontent.com/gd0772/patch/main/gd772.sh
 bash gd772.sh

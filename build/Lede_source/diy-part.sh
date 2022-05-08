34c79a648257bc10dd2652c138365eed7730616b
gd0772 <61563545+gd0772@users.noreply.github.com>
2022-05-09 01:50:17 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -4,4 +4,4 @@
 
 wget https://raw.githubusercontent.com/gd0772/patch/main/gd772.sh
 bash gd772.sh
-sed -i 's/02b79d5e2b07b5e64cd28f1fe84395ee11eef95fc49fd923a9ab93022b148be6/skip/g' feeds/packages/utils/containerd/Makefile
+# sed -i 's/02b79d5e2b07b5e64cd28f1fe84395ee11eef95fc49fd923a9ab93022b148be6/skip/g' feeds/packages/utils/containerd/Makefile

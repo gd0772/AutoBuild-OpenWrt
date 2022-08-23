ca6e93b8dbe5b600ba5431b6b31d4370198cd42c
gd0772 <61563545+gd0772@users.noreply.github.com>
2022-08-23 12:30:18 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -2,8 +2,8 @@
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 # DIY扩展 在此脚本 增加插件
 
-wget https://raw.githubusercontent.com/gd0772/patch/main/gd772.sh
-bash gd772.sh
+wget https://raw.githubusercontent.com/gd0772/patch/main/x86.sh
+bash x86.sh
 
 # python-cryptography
 rm -rf feeds/packages/lang/python/python-cryptography

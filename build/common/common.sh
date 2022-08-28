6dedd7e86ca003e763a395beb1468946bd4dd93d
gd0772 <61563545+gd0772@users.noreply.github.com>
2022-08-29 03:29:34 +0800

    Update common.sh

--- build/common/common.sh
+++ build/common/common.sh
@@ -32,25 +32,10 @@ if [[ ! "${Modelfile}" == "openwrt_amlogic" ]]; then
 	echo -e "\nCONFIG_TARGET_IMAGES_GZIP=y" >> "${PATH1}/${CONFIG_FILE}"
 fi
 if [[ "${Modelfile}" == "openwrt_amlogic" ]]; then
-	# 修复NTFS格式优盘不自动挂载
-	packages=" \
-	brcmfmac-firmware-43430-sdio brcmfmac-firmware-43455-sdio kmod-brcmfmac wpad \
-	kmod-fs-ext4 kmod-fs-vfat kmod-fs-exfat dosfstools e2fsprogs ntfs-3g \
-	kmod-usb2 kmod-usb3 kmod-usb-storage kmod-usb-storage-extras kmod-usb-storage-uas \
-	kmod-usb-net kmod-usb-net-asix-ax88179 kmod-usb-net-rtl8150 kmod-usb-net-rtl8152 \
-	blkid lsblk parted fdisk cfdisk losetup resize2fs tune2fs pv unzip \
-	lscpu htop iperf3 curl lm-sensors python3 luci-app-amlogic
-	"
-	sed -i '/FEATURES+=/ { s/cpiogz //; s/ext4 //; s/ramdisk //; s/squashfs //; }' \
-    		target/linux/armvirt/Makefile
-	for x in $packages; do
-    		sed -i "/DEFAULT_PACKAGES/ s/$/ $x/" target/linux/armvirt/Makefile
-	done
 
 	# luci-app-cpufreq修改一些代码适配amlogic
 	sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' feeds/luci/applications/luci-app-cpufreq/Makefile
-	# 为 armvirt 添加 autocore 支持
-	sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
+	
 fi
 }
 
@@ -64,10 +49,6 @@ find . -name 'luci-app-netdata' -o -name 'netdata' -o -name 'luci-theme-argon' |
 find . -name 'ddns-scripts_aliyun' -o -name 'ddns-scripts_dnspod' -o -name 'luci-app-wol' | xargs -i rm -rf {}
 find . -name 'luci-app-wrtbwmon' -o -name 'wrtbwmon' -o -name 'pdnsd-alt' | xargs -i rm -rf {}
 rm -rf feeds/packages/libs/libcap
-git clone https://github.com/gd0772/package package/gd772
-wget https://raw.githubusercontent.com/gd0772/patch/main/gd772.sh
-bash gd772.sh
-
 }
 
 
@@ -184,24 +165,6 @@ echo
 make defconfig > /dev/null 2>&1
 echo "TIME b \"					插件冲突信息\"" > ${Home}/CHONGTU
 
-if [[ `grep -c "CONFIG_TARGET_ROOTFS_EXT4FS=y" ${Home}/.config` -eq '1' ]]; then
-	if [[ `grep -c "CONFIG_TARGET_ROOTFS_PARTSIZE" ${Home}/.config` -eq '0' ]]; then
-		sed -i '/CONFIG_TARGET_ROOTFS_PARTSIZE/d' ${Home}/.config > /dev/null 2>&1
-		echo -e "\nCONFIG_TARGET_ROOTFS_PARTSIZE=950" >> ${Home}/.config
-	fi
-	egrep -o "CONFIG_TARGET_ROOTFS_PARTSIZE=+.*?[0-9]" ${Home}/.config > ${Home}/EXT4PARTSIZE
-	sed -i 's|CONFIG_TARGET_ROOTFS_PARTSIZE=||g' ${Home}/EXT4PARTSIZE
-	PARTSIZE="$(cat EXT4PARTSIZE)"
-	if [[ "${PARTSIZE}" -lt "950" ]];then
-		sed -i '/CONFIG_TARGET_ROOTFS_PARTSIZE/d' ${Home}/.config > /dev/null 2>&1
-		echo -e "\nCONFIG_TARGET_ROOTFS_PARTSIZE=950" >> ${Home}/.config
-		echo "TIME g \" \"" > ${Home}/EXT4
-		echo "TIME r \"EXT4提示：请注意，您选择了ext4安装的固件格式,而检测到您的分配的固件系统分区过小\"" >> ${Home}/EXT4
-		echo "TIME y \"为避免编译出错,建议修改成950或者以上比较好,已帮您修改成950M\"" >> ${Home}/EXT4
-		echo "TIME g \" \"" >> ${Home}/EXT4
-	fi
-	rm -rf ${Home}/EXT4PARTSIZE
-fi
 if [ -n "$(ls -A "${Home}/Chajianlibiao" 2>/dev/null)" ]; then
 	echo "TIME y \"  插件冲突会导致编译失败，以上操作如非您所需，请关闭此次编译，重新开始编译，避开冲突重新选择插件\"" >>CHONGTU
 	echo "TIME z \"\"" >>CHONGTU
@@ -218,9 +181,6 @@ Diy_chuli() {
 
 sed -i '$ s/exit 0$//' ${Home}/package/base-files/files/etc/rc.local
 echo '
-if [[ `grep -c "coremark" /etc/crontabs/root` -eq '1' ]]; then
-  sed -i '/coremark/d' /etc/crontabs/root
-fi
 /etc/init.d/network restart
 /etc/init.d/uhttpd restart
 exit 0
@@ -234,36 +194,6 @@ fi
 if [[ "${TARGET_BOARD}" == "x86" ]]; then
 	cp -Rf "${Home}"/build/common/Custom/DRM-I915 target/linux/x86/DRM-I915
 	for X in $(ls -1 target/linux/x86 | grep "config-"); do echo -e "\n$(cat target/linux/x86/DRM-I915)" >> target/linux/x86/${X}; done
-fi
-
-if [[ `grep -c "CONFIG_PACKAGE_ntfs-3g=y" ${Home}/.config` -eq '1' ]]; then
-	mkdir -p files/etc/hotplug.d/block && curl -fsSL  https://raw.githubusercontent.com/281677160/openwrt-package/usb/block/10-mount > files/etc/hotplug.d/block/10-mount
-fi
-
-
-if [[ `grep -c "CONFIG_PACKAGE_luci-app-adguardhome=y" ${Home}/.config` -eq '1' ]]; then
-	if [[ `grep -c "CONFIG_ARCH=\"x86_64\"" ${Home}/.config` -eq '1' ]]; then
-		Arch="amd64"
-	elif [[ `grep -c "CONFIG_ARCH=\"i386\"" ${Home}/.config` -eq '1' ]]; then
-		Arch="i386"
-	elif [[ `grep -c "CONFIG_ARCH=\"aarch64\"" ${Home}/.config` -eq '1' ]]; then
-		Arch="arm64"
-	fi
-	if [[ `grep -c "CONFIG_ARCH=\"arm\"" ${Home}/.config` -eq '1' ]]; then
-		if [[ `grep -c "CONFIG_arm_v7=y" ${Home}/.config` -eq '1' ]]; then
-			Arch="armv7"
-		fi	
-	fi
-	if [[ "${Arch}" =~ (amd64|i386|armeb|armv7) ]]; then
-		downloader="curl -L -k --retry 2 --connect-timeout 20 -o"
-		latest_ver="$($downloader - https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest 2>/dev/null|grep -E 'tag_name' |grep -E 'v[0-9.]+' -o 2>/dev/null)"
-		wget -q https://github.com/AdguardTeam/AdGuardHome/releases/download/${latest_ver}/AdGuardHome_linux_${Arch}.tar.gz
-		tar -zxvf AdGuardHome_linux_${Arch}.tar.gz -C ${Home} > /dev/null 2>&1
-		mkdir -p files/usr/bin
-		mv -f AdGuardHome/AdGuardHome files/usr/bin
-		chmod 777 files/usr/bin/AdGuardHome
-		rm -rf {AdGuardHome_linux_${Arch}.tar.gz,AdGuardHome}
-	fi
 fi
 
 if [[ "${BY_INFORMATION}" == "true" ]]; then
@@ -375,16 +305,16 @@ TIME b "仓库地址: ${Github}"
 TIME b "启动编号: #${Run_number}（${CangKu}仓库第${Run_number}次启动[${Run_workflow}]工作流程）"
 TIME b "编译时间: ${Compte}"
 [[ "${Modelfile}" == "openwrt_amlogic" ]] && {
-	TIME g "友情提示：您当前使用【${Modelfile}】文件夹编译【${TARGET_model}】固件"
+	TIME g "您当前使用【${Modelfile}】文件夹编译【${TARGET_model}】固件"
 } || {
-	TIME g "友情提示：您当前使用【${Modelfile}】文件夹编译【${TARGET_PROFILE}】固件"
+	TIME g "您当前使用【${Modelfile}】文件夹编译【${TARGET_PROFILE}】固件"
 }
 echo
 
 if [[ ${UPLOAD_FIRMWARE} == "true" ]]; then
-	TIME y "上传固件在github actions: 开启"
+	TIME y "上传固件到github actions: 开启"
 else
-	TIME r "上传固件在github actions: 关闭"
+	TIME r "上传固件到github actions: 关闭"
 fi
 if [[ ${UPLOAD_CONFIG} == "true" ]]; then
 	TIME y "上传[.config]配置文件: 开启"
@@ -392,14 +322,9 @@ else
 	TIME r "上传[.config]配置文件: 关闭"
 fi
 if [[ ${UPLOAD_BIN_DIR} == "true" ]]; then
-	TIME y "上传BIN文件夹(固件+IPK): 开启"
+	TIME y "上传BIN文件夹: 开启"
 else
-	TIME r "上传BIN文件夹(固件+IPK): 关闭"
-fi
-if [[ ${UPLOAD_COWTRANSFER} == "true" ]]; then
-	TIME y "上传固件至【奶牛快传】和【WETRANSFER】: 开启"
-else
-	TIME r "上传固件至【奶牛快传】和【WETRANSFER】: 关闭"
+	TIME r "上传BIN文件夹: 关闭"
 fi
 if [[ ${UPLOAD_RELEASE} == "true" ]]; then
 	TIME y "发布固件: 开启"
@@ -407,9 +332,9 @@ else
 	TIME r "发布固件: 关闭"
 fi
 if [[ ${SERVERCHAN_SCKEY} == "true" ]]; then
-	TIME y "微信/电报通知: 开启"
+	TIME y "电报通知: 开启"
 else
-	TIME r "微信/电报通知: 关闭"
+	TIME r "电报通知: 关闭"
 fi
 if [[ ${SSH_ACTIONS} == "true" ]]; then
 	TIME y "SSH远程连接: 开启"
@@ -423,13 +348,13 @@ if [[ ${SSHYC} == "true" ]]; then
 	TIME y "SSH远程连接临时开关: 开启"
 fi
 if [[ ${REGULAR_UPDATE} == "true" ]]; then
-	TIME y "把定时自动更新插件编译进固件: 开启"
+	TIME y "固件在线更新: 开启"
 else
-	TIME r "把定时自动更新插件编译进固件: 关闭"
+	TIME r "固件在线更新: 关闭"
 fi
 if [[ ${REGULAR_UPDATE} == "true" ]]; then
 	echo
-	TIME l "定时自动更新信息"
+	TIME l "固件在线更新信息"
 	TIME z "插件版本: ${AutoUpdate_Version}"
 	if [[ ${TARGET_PROFILE} == "x86-64" ]]; then
 		TIME b "传统固件: ${Legacy_Firmware}"
@@ -455,9 +380,8 @@ TIME z "  使用 核心数 为 [ ${CPUCORES} ], 线程数为 [ $(nproc) ]"
 echo
 TIME z "  随机分配到 E5系列CPU 编译是 最慢的, 8171M 快很多，8272CL 的又比 8171M 好些 8370C 最快的！"
 echo
-TIME z "  如果编译的插件较多，而又分配到 E5系列 的 CPU，建议关闭 重新再来！"
-echo
 TIME z "  下面将使用 [ $(nproc) 线程 ] 编译固件"
+echo
 echo
 if [ -n "$(ls -A "${Home}/EXT4" 2>/dev/null)" ]; then
 	echo

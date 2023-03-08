6b56e16ffe5897d0e8914768eb184f6a71033bba
gd0772 <61563545+gd0772@users.noreply.github.com>
2023-03-08 14:57:17 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -5,6 +5,6 @@
 wget https://raw.githubusercontent.com/gd0772/patch/main/x86.sh
 bash x86.sh
 
-#python-cryptography
-#rm -rf feeds/packages/lang/python/python-cryptography
-#svn co https://github.com/openwrt/packages/trunk/lang/python/python-cryptography feeds/packages/lang/python/python-cryptography
+python-cryptography
+rm -rf feeds/packages/lang/python/python-cryptography
+svn co https://github.com/openwrt/packages/trunk/lang/python/python-cryptography feeds/packages/lang/python/python-cryptography
